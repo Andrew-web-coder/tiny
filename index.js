4009fdc0163cb8afdca18390c9eaa5c4ba36d318
Andrew Webcoder <jurciks66@inbox.lv>
2021-06-22 16:07:13 +0300

    update the README to include links to contributing guide

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 module.exports = function tiny(string) {
-  if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
+  if (typeof string !== "string") {
+	throw new TypeError("Tiny wants a string!");
+  }
+
   return string.replace(/\s/g, "");
 };
